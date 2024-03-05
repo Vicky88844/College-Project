4145c9f2a1da09e03e52f2830b2aea635847d7de
Vicky88844 <vikneshrs555@gmail.com>
2024-03-05 16:54:52 +0530

    project

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="App-header">
+        <h1>Hello</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC0Zfawu2cdh4Gluwq9skP35Q2isyAXYyI",
+    authDomain: "movieverse-20fed.firebaseapp.com",
+    projectId: "movieverse-20fed",
+    storageBucket: "movieverse-20fed.appspot.com",
+    messagingSenderId: "829473455872",
+    appId: "1:829473455872:web:e2972546262f8f7f31bde5",
+    measurementId: "G-CR4F261RSV"
+  };
