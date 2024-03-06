5a42bab24c5207a691f4c5572776099699a7a650
Vicky88844 <vikneshrs555@gmail.com>
2024-03-06 15:37:42 +0530

    changes were made

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import HomeScreen from './HomeScreen';
 
 function App() {
+ console.log()
   return (
     <div className="app">
       
         <HomeScreen />
+        
       
     </div>
   );
