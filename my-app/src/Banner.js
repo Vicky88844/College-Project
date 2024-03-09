4bc8256f2b0aff00c5d5e994a0d9bc87384e030a
Vicky88844 <vikneshrs555@gmail.com>
2024-03-09 17:44:17 +0530

    phase 1

---
+++ my-app/src/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Banner.css";
+
+function Banner() {
+  return (<header className='banner'style={{
+    backgroundImage: 'url()'
+  }}>
+
+  </header> )
+}
+
+export default Banner
--- my-app/src/HomeScreen.js
+++ my-app/src/HomeScreen.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import  "./HomeScreen.css";
-import Nav from './Nav';
-
+import React from 'react';
+import Banner from "./Banner";
+import "./HomeScreen.css";
+import Nav from "./Nav";
 function HomeScreen() {
   return (
     <div className="homeScreen">
 
         <Nav />
+   
 
 
 
-        {/* Banner */}
+      <Banner />
 
 
 
