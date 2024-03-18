f2f0afc20adbfded524775bd1fed6001ae4b5bce
Vicky88844 <vikneshrs555@gmail.com>
2024-03-18 10:45:39 +0530

    API integeration

--- my-app/src/axios.js
+++ my-app/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 const instance =axios.create({
-    baseURL:"https://api.themoviedb.org/3/discover/movie",
+    baseURL:"https://www.themoviedb.org/3",
 });
 export default instance;
