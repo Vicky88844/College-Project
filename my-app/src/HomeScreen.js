e3f61a021b7042470f2ad1469c10016a82220f59
Vicky88844 <vikneshrs555@gmail.com>
2024-03-23 14:19:30 +0530

    rows implemented

--- my-app/src/HomeScreen.js
+++ my-app/src/HomeScreen.js
@@ -17,7 +17,7 @@ function HomeScreen() {
 
 
 
-       <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} isLarge />
+       <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
        <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
        <Row title='Action Movies' fetchUrl={requests.fetchActionMovies}/>
        <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies}/>
