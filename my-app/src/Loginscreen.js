21a6f75f67eafbd2a8c50ad0bd6eb2a23869f69b
Vicky88844 <vikneshrs555@gmail.com>
2024-03-30 22:29:55 +0530

    login page

--- my-app/src/Loginscreen.js
+++ my-app/src/Loginscreen.js
@@ -6,13 +6,25 @@ function Loginscreen() {
     <div className='loginscreen'>
         <div className='loginscreen-background'>
         <img 
-        className='loginscreen-logo'
+        className='loginscreen-logo' alt=''
         src={LOGO}>
        </img>
        <button className='loginscreen-button'>
            Sign In</button>
 
-         <div className='loginscreen-gradient' />  
+         <div className="loginscreen-gradient" />  
+        </div>
+        <div className='loginscreen-body'>
+          <>
+          <h1>Unlimited films, Tv programmes and
+          more.</h1>
+            <h2>Watch anywhere. Cancel at any time.</h2> 
+            <h3>
+              Ready to watch? Enter your email to 
+              create or restart your
+              membership. 
+            </h3>
+          </>
         </div>
     </div>
   )
