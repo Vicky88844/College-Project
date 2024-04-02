a2a4d3d6f87001ee7085bfe8907982d22f8798a1
Vicky88844 <vikneshrs555@gmail.com>
2024-04-02 09:29:50 +0530

    login page

--- my-app/src/SignUpScreen.js
+++ my-app/src/SignUpScreen.js
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './SignUpScreen.css';
-function SignUpScreen() {
+import { auth } from './firebase';
+function SignUpScreen() { 
+  const emailRef=useRef(null);
+  const passwordRef=useRef(null);
+
+ const register = (e) => {
+  e.preventDefault();
+
+  auth.createUserWithEmailAndPassword(
+    emailRef.current.value,
+    passwordRef.current.value
+  )
+  .then((authUser) => {
+    console.log(authUser);
+
+  })
+  .catch((error) => {
+    alert(error.message);
+  });
+ };
+ 
+ const signIn = (e) =>{
+  e.preventDefault();
+ };
+  
   return (
     <div className='signupScreen'>
-        
-              <form>
-                <h1>Sign In</h1>
-    <input placeholder='Email' type='email'/>
-            <input placeholder='Password' type='password' />
-            <button type='submit'>Sign In</button>
-                </form>
-    </div>
+    <form>
+    <h1>Sign In</h1>
+    <input ref={emailRef} placeholder='Email' type='email'/>
+          <input ref={passwordRef} placeholder='Password' type='password' />
+          <button type='submit'onClick={register}>Sign In</button>
+          <h4>
+           <span className='signupScreen-grey'> New to Movieverse? </span>
+           <span className='signupScreen-link'onClick={signIn}>Sign Up now</span>
+           
+           </h4>
+      </form>
+             </div>
   )
 }
 
--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -1,4 +1,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from './firebase';
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyC0Zfawu2cdh4Gluwq9skP35Q2isyAXYyI",
     authDomain: "movieverse-20fed.firebaseapp.com",
@@ -8,3 +11,10 @@ const firebaseConfig = {
     appId: "1:829473455872:web:e2972546262f8f7f31bde5",
     measurementId: "G-CR4F261RSV"
   };
+
+  const firebaseApp =firebase.initializeApp(firebaseConfig);
+  const db =firebaseApp.firestore();
+  const auth =firebase.auth();
+
+  export { auth };
+  export default db;
