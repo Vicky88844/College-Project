df60acd5d079c6f2b2f39d656d709c713f6e5afd
Vicky88844 <vikneshrs555@gmail.com>
2024-04-04 20:27:07 +0530

    trailer function added

--- my-app/src/Banner.js
+++ my-app/src/Banner.js
@@ -4,48 +4,73 @@ import axios from "./axios";
 import requests from "./Request";
 
 function Banner() {
-  const [movie,setmovie]= useState([]);
+  const [movie, setMovie] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState('');
 
   useEffect(() => {
     async function fetchData() {
-      const request = await axios.get(requests.fetchNetflixOriginals);
-      setmovie(
-        request.data.results[
-          Math.floor(Math.random() * request.data.results.length -1)
-        ]
-      );
-      return request;
+      try {
+        const request = await axios.get(requests.fetchNetflixOriginals);
+        setMovie(
+          request.data.results[
+            Math.floor(Math.random() * request.data.results.length - 1)
+          ]
+        );
+      } catch (err) {
+        console.error('Movie fetch error:', err);
+      }
     }
-      fetchData();
-},[])
+    fetchData();
+  }, []);
 
-console.log(movie);
+  useEffect(() => {
+    if (movie?.id) {
+      async function fetchTrailer() {
+        try {
+          const response = await axios.get(`/movie/${movie.id}/videos?api_key=dbc825f980685e71caf4ee196227479c`);
+          const trailerKey = response.data.results.find((video) => video.type === "Trailer")?.key;
+          setTrailerUrl(trailerKey);
+        } catch (err) {
+          console.error('Trailer fetch error:', err);
+        }
+      }
+      fetchTrailer();
+    }
+  }, [movie]);
 
-  function truncate(string, n){
-    return string?.length > n ? string.substr(0,n-1) + '...' : string;
+  const handleClick = () => {
+    if (trailerUrl) {
+      const youtubeUrl = `https://www.youtube.com/watch?v=${trailerUrl}`;
+      window.open(youtubeUrl, '_blank');
+    } else {
+      console.error('No trailer URL available');
+    }
+  };
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + '...' : str;
   }
-  return (<header className='banner'style={{
-    backgroundSize:"cover",
-    backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-    backgroundPosition: "center center",
-  }} >
-    <div className='banner-contents'>
-      <h1 className='banner-title'>
-        {movie?.title || movie?.name || movie?.original_name}
-      </h1>
-      <div className='banner-buttons'>
-        <button className='banner-button'>Play</button>
-        <button className='banner-button'>My List</button>
+
+  return (
+    <header className='banner' style={{
+      backgroundSize: "cover",
+      backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+      backgroundPosition: "center center",
+    }}>
+      <div className='banner-contents'>
+        <h1 className='banner-title'>
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className='banner-buttons'>
+          <button className='banner-button' onClick={handleClick}>Play</button>
+        </div>
+        <h1 className='banner-description'>
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
-      <h1 className='banner-description'>
-        {truncate(movie?.overview , 150)}
-        
-
-
-      </h1>
-    </div>
-    <div className='banner--fadebutton' />
-  </header> )
+      <div className='banner--fadebutton' />
+    </header>
+  );
 }
 
-export default Banner
+export default Banner;
--- my-app/src/Row.js
+++ my-app/src/Row.js
@@ -1,44 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import "./Row.css";
 import axios from './axios';
-function Row({title,fetchUrl,isLargeRow = false}) {
-    const [movies,setMovies] = useState([]);
 
-    const base_url = "https://image.tmdb.org/t/p/original/"
+function Row({ title, fetchUrl, isLargeRow = false }) {
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-     },[fetchUrl]);
+  const base_url = "https://image.tmdb.org/t/p/original/";
 
-    
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  const handleClick = async (movie) => {
+    try {
+      const response = await axios.get(`/movie/${movie.id}/videos?api_key=dbc825f980685e71caf4ee196227479c`);
+      const trailerKey = response.data.results[0]?.key;
+      if (trailerKey) {
+        const youtubeUrl = `https://www.youtube.com/watch?v=${trailerKey}`;
+        window.open(youtubeUrl, '_blank', 'fullscreen=yes');
+      } else {
+        throw new Error('Trailer not found');
+      }
+    } catch (error) {
+      console.error('Error fetching trailer:', error);
+    }
+  };
+
   return (
     <div className='row'>
-        <h2>{title}</h2>
+      <h2>{title}</h2>
       <div className="row-posters">
-      
-      {movies.map(
-      (movie) =>(
-          ((isLargeRow && movie.poster_path) || 
-          (!isLargeRow && movie.backdrop_path ))&& (
-            <img 
-           
-          className={`row-poster ${isLargeRow && "row-posterLarge"}`}
-          key={movie.id}
-          src={`${base_url}${
-            isLargeRow ? movie.poster_path : movie.backdrop_path
-          }`}
-           alt={movie.name}/>
-        ))
-      )}
-          
-
-        </div>
-       
+        {movies.map(movie => (
+          ((isLargeRow && movie.poster_path) || (!isLargeRow && movie.backdrop_path)) && (
+            <img
+              className={`row-poster ${isLargeRow ? "row-posterLarge" : ""}`}
+              key={movie.id}
+              src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+              alt={movie.name}
+              onClick={() => handleClick(movie)}
+            />
+          )
+        ))}
+      </div>
     </div>
   );
 }
